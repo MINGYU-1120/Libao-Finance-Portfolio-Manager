d60c6942dd21e30c4d3cb1697821e8c3ba7c7293
MINGYU-1120 <bboylilming@gmail.com>
2026-02-10 16:36:57 +0800

    Switch API handler to ESM syntax

--- api/kajabi-webhook/index.js
+++ api/kajabi-webhook/index.js
@@ -1,8 +1,8 @@
-module.exports = (req, res) => {
+export default function handler(req, res) {
     res.status(200).json({
         ok: true,
-        message: 'Kajabi Webhook Endpoint is Active',
+        message: 'Kajabi Webhook Endpoint is Active (ESM)',
         query: req.query,
         body: req.body
     });
-};
+}
